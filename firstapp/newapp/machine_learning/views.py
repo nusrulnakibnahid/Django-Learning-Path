5a34b5abf72beba9e6051b0095a6d004197166ca
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-03-01 23:00:25 +0600

    first commit

---
+++ firstapp/newapp/machine_learning/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def machine(request):
+    return HttpResponse("This is machine learning page")
