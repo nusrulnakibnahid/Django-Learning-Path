48e7af91c3f7e917c2ad3c14a9a57eb179a077ea
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-03-28 23:46:23 +0600

    MakeMigrations & Migrate

--- aiquest/newproject/About_Us/models.py
+++ aiquest/newproject/About_Us/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Teachers(models.Model):
+    tid = models.ImageField()
+    tname = models.CharField(max_length=40)
+    temail = models.EmailField(max_length=30)
+
