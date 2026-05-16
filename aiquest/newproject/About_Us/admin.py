54769d545e97ce7cd75dc95dd3e58242f7a0cfaa
nusrulnakibnahid <nusrulnakibnahid.mail@gmail.com>
2026-05-16 23:27:43 +0600

    Refactor Teachers model and admin registration: change tid field to IntegerField, update admin class for better display

--- aiquest/newproject/About_Us/admin.py
+++ aiquest/newproject/About_Us/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from About_Us.models import Teachers
 
 # Register your models here.
+class TeachersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tid', 'tname', 'temail')
+admin.site.register(Teachers, TeachersAdmin)
--- aiquest/newproject/About_Us/models.py
+++ aiquest/newproject/About_Us/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Teachers(models.Model):
-    tid = models.ImageField()
+    tid = models.IntegerField()
     tname = models.CharField(max_length=50)
     temail = models.EmailField(max_length=30)
 
